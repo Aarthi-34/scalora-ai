17b4f53715ddaa929198b922db9104ab2970cb3e
Aarthi <aarthymani34@gmail.com>
2026-03-11 23:16:33 +0530

    Organized frontend files into public folder

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Saclora AI is running!");
-});
+app.use(express.static("public"));
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
